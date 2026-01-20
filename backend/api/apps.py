65950772fe043a7eaf730486dbb2d1fa831eff48
minhhoang0305 <148510548+minhhoang0305@users.noreply.github.com>
2026-01-20 19:25:16 +0700

    Fix database init error and remove DB access from AppConfig

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -3,10 +3,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     name = "api"
 
-    def ready(self):
-        from django.contrib.auth.models import User
-        if not User.objects.filter(username="user_qRPY").exists():
-            User.objects.create_user(
-                username="hoang",
-                password="123456"
-            )
